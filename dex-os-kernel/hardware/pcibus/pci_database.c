8460179fa04be52ed3ead79b04b31d07ae84f655
jedld <jedld>
2004-04-17 09:27:23 +0000

    *** empty log message ***

---
+++ dex-os-kernel/hardware/pcibus/pci_database.c
@@ -0,0 +1,17 @@
+
+pci_vendor	pci_vendors[] =
+{
+	{ 0xF5F5, "", "F5 Networks Inc." } ,
+	{ 0xFA57, "FAST", "Fast Search & Transfer" } ,
+	{ 0x0,"Unknown","Unknown"}
+} ;
+
+
+
+pci_device	pci_devices[] =
+{
+	{ 0x0,0x0,"Unknown","Unknown"}
+} ;
+
+
+
