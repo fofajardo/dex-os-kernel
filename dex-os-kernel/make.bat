713e3bae7044ae55852f82611e7ace6971885c42
jedld <jedld>
2006-05-27 14:35:17 +0000

    Made directory paths environment variables for easier maintenance

--- dex-os-kernel/make.bat
+++ dex-os-kernel/make.bat
@@ -1,21 +1,31 @@
+REM Set the location of your DJGPP dev environment here
+SET DJGPP_LOC=C:\gcc
+SET NASM_LOC=C:\gcc\bin
+
+
 del kernel32.bin
 del kernel32.o
 del fat.o
 
 REM set this to the location of djgpp.env
-set DJGPP=C:\gcc\djgpp.env
+set DJGPP=%DJGPP_LOC%\djgpp.env
 
-%1\gcc -fwritable-strings -nostdlib -fno-builtin -ffreestanding -c -o kernel32.o kernel32.c
-pause
-%1\gcc -w -fwritable-strings -nostdlib -fno-builtin -ffreestanding -c -o scheduler.o process\scheduler.c
-%1\gcc -w -fwritable-strings -nostdlib -fno-builtin -ffreestanding -c -o fat.o filesystem\fat12.c
-%1\gcc -w -fwritable-strings -nostdlib -fno-builtin -ffreestanding -c -o iso9660.o filesystem\iso9660.c
-%1\gcc -w -fwritable-strings -nostdlib -fno-builtin -ffreestanding -c -o devfs.o filesystem\devfs.c
-%1\gcc -w -fwritable-strings -nostdlib -fno-builtin -ffreestanding -c -o iomgr.o iomgr\iosched.c
-%1\gcc -w -fwritable-strings -nostdlib -fno-builtin -ffreestanding -c -o devmgr_error.o devmgr\devmgr_error.c
-%2\nasmw -f coff -o startup.o startup\startup.asm
-%2\nasmw -f coff -o asmlib.o startup\asmlib.asm
-%2\nasmw -f coff -o irqwrap.o irqwrap.asm
-%1\ld -T lscript.txt -Map mapfile.txt
+%DJGPP_LOC%\bin\gcc -w -nostdlib -fno-builtin -ffreestanding -c -o kernel32.o kernel32.c
+
+%DJGPP_LOC%\bin\gcc -w  -nostdlib -fno-builtin -ffreestanding -c -o scheduler.o process\scheduler.c
+
+%DJGPP_LOC%\bin\gcc -w  -nostdlib -fno-builtin -ffreestanding -c -o fat.o filesystem\fat12.c
+
+%DJGPP_LOC%\bin\gcc -w  -nostdlib -fno-builtin -ffreestanding -c -o iso9660.o filesystem\iso9660.c
+
+%DJGPP_LOC%\bin\gcc -w  -nostdlib -fno-builtin -ffreestanding -c -o devfs.o filesystem\devfs.c
+
+%DJGPP_LOC%\bin\gcc -w  -nostdlib -fno-builtin -ffreestanding -c -o iomgr.o iomgr\iosched.c
+
+%DJGPP_LOC%\bin\gcc -w  -nostdlib -fno-builtin -ffreestanding -c -o devmgr_error.o devmgr\devmgr_error.c
+%NASM_LOC%\nasmw -f coff -o startup.o startup\startup.asm
+%NASM_LOC%\nasmw -f coff -o asmlib.o startup\asmlib.asm
+%NASM_LOC%\nasmw -f coff -o irqwrap.o irqwrap.asm
+%DJGPP_LOC%\bin\ld -T lscript.txt -Map mapfile.txt
 REM -Ttext 0x100000 --oformat binary -o kernel32.bin startup.o irqwrap.o asmlib.o kernel32.o scheduler.o
 dir kernel32.bin
